a08a3b355d2c5a23d59c22e48118d2f50c7823b8
Your Name <you@example.com>
2022-01-07 02:10:06 +0100

    debugging

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,12 @@ use sha2::{Sha256, Sha512, Digest};
 fn hash(input: Vec<u8>) -> Vec<u8> {
     let mut input = input.clone();
     let secret_base64 = env::var("AUTH_SERVER_SECRET").unwrap();
+
+    println!("{:?}", &input);
+    println!("{:?}", &secret_base64);
     let mut secret_bytes = decode(&secret_base64).unwrap();
+    println!("{:?}", &secret_bytes);
+
     input.append(&mut secret_bytes);
     let mut hasher = Sha256::new();
     hasher.update(&input);
@@ -21,9 +26,13 @@ fn hash(input: Vec<u8>) -> Vec<u8> {
 
 fn validate_cookie(cookie_base64: String) -> Result<bool, DecodeError> {
     let cookie = decode(&cookie_base64)?;
+    println!("{:?}", &cookie);
     let cookie_token = &cookie[0..8];
+    println!("{:?}", &cookie_token);
     let cookie_hash: Vec<u8> = (&cookie[8..24]).to_vec();
+    println!("{:?}", &cookie_hash);
     let token_hash = hash((&cookie_token).to_vec());
+    println!("{:?}", &token_hash);
     Ok(token_hash == cookie_hash)
 }
 
@@ -37,10 +46,11 @@ fn generate_cookie() -> String {
 
 #[get("/{token}")]
 async fn auth(web::Path(token): web::Path<String>) -> Result<HttpResponse> {
+    println!("auth: {}", &token);
     let domain = env::var("DOMAIN_NAME").unwrap();
     match validate_cookie(token.clone()) {
         Ok(true) => Ok(
-            HttpResponse::Ok()
+            HttpResponse::Found()
             .cookie(
                 http::Cookie::build("auth_token", &token)
                 .domain(domain)
@@ -49,6 +59,7 @@ async fn auth(web::Path(token): web::Path<String>) -> Result<HttpResponse> {
                 .http_only(true)
                 .finish()
             )
+            .header(http::header::LOCATION, "/")
             .finish()
         ),
         _ => Ok(
@@ -59,11 +70,14 @@ async fn auth(web::Path(token): web::Path<String>) -> Result<HttpResponse> {
 
 #[get("/")]
 async fn index(req: HttpRequest) -> Result<HttpResponse> {
+    println!("index");
     if let Some(c) = req.cookie("auth_token") {
        if let Ok(_) = validate_cookie(c.value().to_string()) {
+           println!("qqqindex");
            return Ok(HttpResponse::Ok().finish());
        }
     }
+    println!("wwwindex");
     return Ok(HttpResponse::Forbidden().finish());
 }
 
